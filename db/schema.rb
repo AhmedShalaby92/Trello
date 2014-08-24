1251b17e032f0e25a76b94bbb9593bf06f65505a
Ahmed Shalaby <ahmed.shalaby.92@gmail.com>
2014-08-24 16:58:55 +0300

    customize devise and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822093923) do
+ActiveRecord::Schema.define(version: 20140824125104) do
 
   create_table "members", force: true do |t|
     t.string   "user_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20140822093923) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
