d12b459cd0b7a9c1c492f427bd897996430b7ca6
Motaz Elshaer <motazelshaer@Motazs-Mac-mini.local>
2014-08-25 11:17:41 +0300

    modifying git ignore file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140824122750) do
+ActiveRecord::Schema.define(version: 20140824125104) do
+
   create_table "members", force: true do |t|
     t.string   "user_id"
     t.text     "project_id"
